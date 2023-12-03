7d85ad5d276ca48648784251632d29845132bd25
Iosif Vieru <iosif.vieru@student.tuiasi.ro>
2023-12-03 16:55:13 +0200

    updates

---
+++ transaction.py
@@ -0,0 +1,33 @@
+import user
+import database
+
+class Transaction:
+    def borrowBook(book, user):
+        bookQuantity = book.getQuantity()
+
+        if(bookQuantity <= 0):
+            pass
+        
+        tempObject = book.createCopy()
+
+        user.borrowBook(tempObject)
+
+        # sql
+        sql = f"""
+            INSERT INTO borrowedBooks (user_id, book_id) VALUES ('{user.getID()}', '{tempObject.getId()}')
+        """
+        database.query(sql)
+
+        book.updateQuantity(bookQuantity - 1)
+
+        # ..
+
+    def returnBook(book, user):
+        # delete din db
+        # increment la cantitate in db
+        sql = f"""
+            DELETE FROM borrowedBooks WHERE user_id = '{user.getID()}' and book_id = '{book.getId()}'
+        """
+        database.query(sql)
+        bookQuantity = book.getQuantity()
+        book.updateQuantity(bookQuantity + 1)
